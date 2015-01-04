5674350aeca1cbe7cdef9d1f6ae3ebf24298b51f
wrwcmaster <wrwcfanscott@gmail.com>
2015-01-05 01:05:12 +0800

    added http multipart form modifier

---
+++ CommonLib/Net/Http/RequestModifier/HttpRequestMultipartFormModifier.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaia.CommonLib.Net.Http.RequestModifier
+{
+    public class HttpRequestMultipartFormModifier : AbstractHttpRequestModifier
+    {
+        public class FileInfo
+        {
+            public string FileName { get; set; }
+            public Func<Stream> FileStreamProvider { get; set; }
+
+            public FileInfo(string filePath)
+            {
+                if (filePath == null) throw new ArgumentNullException("filePath");
+                FileName = Path.GetFileName(filePath);
+                FileStreamProvider = () => new FileStream(filePath, FileMode.Open);
+            }
+        }
+
+        private IEnumerable<KeyValuePair<string, string>> _parameters;
+        private IEnumerable<KeyValuePair<string, FileInfo>> _fileFields;
+        private String _boundary;
+        private String BoundaryMultiPart { get { return "--" + _boundary; } }
+        private String BoundaryEnd { get { return "--" + _boundary + "--"; } }
+        public Encoding Encoding { get; set; }
+        public HttpRequestMultipartFormModifier(IEnumerable<KeyValuePair<string, string>> parameters, IEnumerable<KeyValuePair<string, FileInfo>> fileFields)
+        {
+            Encoding = UTF8Encoding.UTF8;
+            _boundary = System.Guid.NewGuid().ToString();
+            _parameters = parameters;
+            _fileFields = fileFields;
+        }
+        
+        public override IList<IHttpRequestHeaderModifier> HeaderModifiers
+        {
+            get
+            {
+                return new List<IHttpRequestHeaderModifier>(){
+                    new HttpRequestHeaderModifier("Content-Type", "multipart/form-data; boundary=" + _boundary)
+                };
+            }
+        }
+
+        public override void WriteBodyContent(Stream requestStream)
+        {
+            WriteParamBodyParts(requestStream);
+            WriteFileBodyParts(requestStream);
+            WriteText(requestStream, BoundaryEnd);
+        }
+
+        private void WriteText(Stream requestStream, string text)
+        {
+            byte[] contents = Encoding.GetBytes(text);
+            requestStream.Write(contents, 0, contents.Length);//TODO: use buffer
+        }
+
+        private void WriteParamBodyParts(Stream requestStream)
+        {
+            foreach(var parameter in _parameters){
+                WriteText(requestStream, String.Format("{0}\r\nContent-Disposition: form-data; name=\"{1}\"\r\n\r\n{2}\r\n", BoundaryMultiPart, parameter.Key ?? "", parameter.Value ?? ""));
+            }
+        }
+
+        private void WriteFileBodyParts(Stream requestStream)
+        {
+            foreach (var fileField in _fileFields)
+            {
+                if(fileField.Value != null){
+                    string fieldName = fileField.Key;
+                    string fileName = fileField.Value.FileName;
+                    WriteText(requestStream, String.Format("{0}\r\nContent-Disposition: form-data; name=\"{1}\"; filename=\"{2}\"\r\nContent-Type: application/octet-stream; charset={3}\r\n\r\n", BoundaryMultiPart, fileField.Key ?? "", fileName ?? "TempFile", Encoding.WebName));
+                    using(Stream fileStream = fileField.Value.FileStreamProvider.Invoke()){
+                        fileStream.CopyTo(requestStream); //TODO: support IExecutionControl
+                    }
+                    WriteText(requestStream, "\r\n");
+                }            
+            }
+        }
+    }
+}
---
+++ CommonLib/Net/Http/ResponseParser/IHttpResponseParser.cs
@@ -0,0 +1,15 @@
+﻿using Gaia.CommonLib.Execute.Control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaia.CommonLib.Net.Http.ResponseParser
+{
+    public interface IHttpResponseParser<T>
+    {
+        T ParseHttpResponse(HttpWebResponse response, IExecutionControl control);
+    }
+}
