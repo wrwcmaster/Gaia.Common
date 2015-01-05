439652918df86e6b862bd4f9a6af84e79a4a79ee
wrwcmaster <wrwcfanscott@gmail.com>
2015-01-05 22:51:13 +0800

    Added HttpResponseJSONObjectParser

---
+++ CommonLib/Net/Http/ResponseParser/HttpResponseJSONObjectParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaia.CommonLib.Net.Http.ResponseParser
+{
+    public class HttpResponseJSONObjectParser<T> : IHttpResponseParser<T>
+    {
+        
+        public HttpResponseJSONObjectParser()
+        {
+            
+        }
+
+        public T ParseHttpResponse(System.Net.HttpWebResponse response, Execute.Control.IExecutionControl control)
+        {
+            using (Stream s = response.GetResponseStream())
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+                return (T)serializer.ReadObject(s);
+            }
+        }
+    }
+}
