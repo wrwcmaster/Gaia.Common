363ccbe1edbd15067cd5bc115f6f7ca0e7716dad
wrwcmaster <wrwcfanscott@gmail.com>
2015-01-06 00:24:50 +0800

    Added CollectionExtensions - KeyValuePairList

---
+++ CommonLib/KeyValuePairList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaia.CommonLib
+{
+    public class KeyValuePairList<TKey, TValue> : List<KeyValuePair<TKey, TValue>>
+    {
+        public void Add(TKey key, TValue value)
+        {
+            this.Add(new KeyValuePair<TKey, TValue>(key, value));
+        }
+    }
+}
