03cf27741eef156ca9750a48ee9e98a06286e533
wrwcmaster <wrwcfanscott@gmail.com>
2015-02-23 01:54:04 +0800

    added Enumerable extension

---
+++ CommonLib/Collections/IEnumerableExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaia.Common.Collections
+{
+    public static class IEnumerableExtension
+    {
+        public static IEnumerable GetSafeEnumerable(this IEnumerable obj)
+        {
+            if (obj == null) return new object[0];
+            else return obj;
+        }
+
+        public static IEnumerable<T> GetSafeEnumerable<T>(this IEnumerable<T> obj)
+        {
+            if (obj == null) return new T[0];
+            else return obj;
+        }
+    }
+}
